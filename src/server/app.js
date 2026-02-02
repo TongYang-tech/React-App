28493ca44948a994f6955bcb7acf230d528648fd
Tong Yang <tongyang80@yahoo.com>
2026-02-01 22:52:07 -0600

    fix: cleanup.

--- src/server/app.js
+++ src/server/app.js
@@ -1,15 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import router from './route/index.js'
 
 const app = express();
 
 app.use(express.json())
-app.get("/health", (_, res) => {
-  res.status(200).json({
-    "data": "OK",
-    "timestamp": new Date().toISOString()
-  })
-});
+app.use(router)
 
 app.use(cors({
   origin: 'http://localhost:3000',
