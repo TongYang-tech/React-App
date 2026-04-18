bc23829f443aa10273f2058b30270505867ea9f3
Tong Yang <tongyang80@yahoo.com>
2026-04-17 23:06:34 -0500

    fix: remove api fetch.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,22 +1,6 @@
-import { useEffect } from 'react'
-import axiosClient from './utils/axiosHelper'
 import './App.css'
 
 function App() {
-  const getHealth = async () => {
-    try {
-      const resp = await axiosClient.get(`/placeholder/api/health`)
-      const data = resp.data
-      console.log(data)
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-    getHealth()
-  }, [])
-
   return (
     <>
       Hello World!
